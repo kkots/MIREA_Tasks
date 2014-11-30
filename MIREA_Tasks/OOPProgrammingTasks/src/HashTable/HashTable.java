fd70d7d4a0a6fbc55062031aad1934ced4a8e4b2
kkots <okknots@gmail.com>
2014-12-01 01:07:48 +0400

    Made memory usage more effective

--- MIREA_Tasks/OOPProgrammingTasks/src/HashTable/HashTable.java
+++ MIREA_Tasks/OOPProgrammingTasks/src/HashTable/HashTable.java
@@ -1,36 +1,54 @@
 package HashTable;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
-public class HashTable implements MyMap<Integer, Integer> {
-	private ArrayList<Integer> list;
+public class HashTable implements MyMap<String, Integer> {
+	//guaranteed to use minimum amounts of memory
+	private String[] indexes;
+	private Integer[] values;
+	//indexes.length == values.length ALWAYS
 	
 	public HashTable() {
-		list = new ArrayList<Integer>();
-	}
-	@Override
-	public Integer get(Integer key) {
-		return list.get(key);
-	}
-
-	@Override
-	public void put(Integer key, Integer value) {
-		list.ensureCapacity(key);
-		list.set(key, value);
-	}
-
-	@Override
-	public void remove(Integer key) {
-		list.remove(key);
-		list.trimToSize();
+		indexes = new String[0];
+		values = new Integer[0];
 	}
 
 	@Override
 	public int size() {
-		int count = 0;
-		for (Integer a : list) {
-			if (a != null) count++;
+		return indexes.length;
+	}
+	@Override
+	public Integer get(String key) {
+		for (int i = 0; i < indexes.length; i++) {
+			if (indexes[i].equals(key)) return values[i];
 		}
-		return count;
+		return null;
+	}
+	@Override
+	public void put(String key, Integer value) {
+		for (int i = 0; i < indexes.length; i++) {
+			if (indexes[i].equals(key)) {
+				values[i] = value;
+				return;
+			}
+		}
+		indexes = Arrays.copyOf(indexes, indexes.length + 1);
+		values = Arrays.copyOf(values, values.length + 1);
+		indexes[indexes.length - 1] = key;
+		values[values.length - 1] = value;
+	}
+	@Override
+	public void remove(String key) {
+		for (int i = 0; i < indexes.length; i++) {
+			if (indexes[i].equals(key)) {
+				for (int j = i; j < indexes.length - 1; j++) {
+					indexes[j] = indexes[j + 1];
+					values[j] = values[j + 1];
+				}
+				indexes = Arrays.copyOf(indexes, indexes.length - 1);
+				values = Arrays.copyOf(values, values.length - 1);
+				return;
+			}
+		}
 	}
 }
